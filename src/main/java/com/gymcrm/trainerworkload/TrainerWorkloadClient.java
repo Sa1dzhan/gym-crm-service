cc2d48fae64e414b0ad34631aa3e83990b1be1f2
Sa1dzhan <zhansaid@gmail.com>
2025-06-11 17:35:14 +0500

    minor fix

--- src/main/java/com/gymcrm/trainerworkload/TrainerWorkloadClient.java
+++ src/main/java/com/gymcrm/trainerworkload/TrainerWorkloadClient.java
@@ -1,43 +1,12 @@
 package com.gymcrm.trainerworkload;
 
-import com.gymcrm.config.JwtUtil;
 import com.gymcrm.dto.workload.DurationRequestDto;
 import com.gymcrm.dto.workload.DurationResponseDto;
 import com.gymcrm.dto.workload.WorkloadRequestDto;
-import com.gymcrm.trainerworkload.read.WorkloadReadSummaryClient;
-import com.gymcrm.trainerworkload.update.WorkloadUpdateSummaryClient;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class TrainerWorkloadClient {
+public interface TrainerWorkloadClient {
+    ResponseEntity<?> updateTrainerSummary(WorkloadRequestDto request);
 
-    private final WorkloadUpdateSummaryClient updateClient;
-
-    private final WorkloadReadSummaryClient readClient;
-
-    private final JwtUtil jwtUtil;
-
-    private final String service = "workload";
-
-    public ResponseEntity<?> updateTrainerSummary(WorkloadRequestDto request) {
-        String transactionId = MDC.get("transactionId");
-        String authToken = jwtUtil.generateToken(service.concat(request.getUsername()));
-
-        log.info("Sending workload update request for: {}", request.getUsername());
-        return updateClient.updateTrainerSummary(request, authToken, transactionId);
-    }
-
-    public ResponseEntity<DurationResponseDto> getTrainerWorkload(DurationRequestDto request) {
-        String transactionId = MDC.get("transactionId");
-        String authToken = jwtUtil.generateToken(request.getUsername());
-
-        log.info("Requesting workload summary for: {}", request.getUsername());
-        return readClient.getTrainerWorkload(request, authToken, transactionId);
-    }
+    ResponseEntity<DurationResponseDto> getTrainerWorkload(DurationRequestDto request);
 }
